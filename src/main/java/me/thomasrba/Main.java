b19caaf8fde2440efdc22688a3199bfe9daa563d
Thomas <thomasribalta@gmail.com>
2024-03-25 01:32:44 +0100

    Start Project

--- src/main/java/me/thomasrba/Main.java
+++ src/main/java/me/thomasrba/Main.java
@@ -1,16 +1,23 @@
 package me.thomasrba;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import me.thomasrba.commandStaff.CommandStaff;
+
+import java.util.Objects;
 
 public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getLogger().info("&9&lPluginStaff &aActivation...");
+        getLogger().info("PluginStaff Activation...");
+        Objects.requireNonNull(getCommand("gmc")).setExecutor(new CommandStaff());
+        Objects.requireNonNull(getCommand("gms")).setExecutor(new CommandStaff());
+        Objects.requireNonNull(getCommand("ban")).setExecutor(new CommandStaff());
+        Objects.requireNonNull(getCommand("kick")).setExecutor(new CommandStaff());
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("&9&lPluginStaff &cDesactivation...");
+        getLogger().info("PluginStaff Desactivation...");
     }
 }
